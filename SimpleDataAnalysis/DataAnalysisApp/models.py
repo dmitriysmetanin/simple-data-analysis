d8ef201ff053116240986a02311723f3dc8e3ec0
Dmitriy <88580214+dmitriysmetanin@users.noreply.github.com>
2023-12-03 17:25:14 +0300

    добавил модель "модель сравнения"

--- SimpleDataAnalysis/DataAnalysisApp/models.py
+++ SimpleDataAnalysis/DataAnalysisApp/models.py
@@ -6,4 +6,11 @@ class Feedback(models.Model):
     contact = models.CharField(max_length=255)
     status = models.IntegerField(default=0)
 
+class ComparisonModelFeature(models.Model):
+    name = models.CharField(max_length=255)
+    data_delimiter = models.CharField(max_length=16)
+    data = models.TextField()
 
+class ComparisonModel(models.Model):
+    name = models.CharField(max_length=255)
+    features = models.ManyToOneRel(ComparisonModelFeature)
